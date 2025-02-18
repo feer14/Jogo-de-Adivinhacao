34eeeaa381ea12b306f50e5f10d105b012785695
feer14 <maria.motta.lopes@escola.pr.gov.br>
2025-02-18 08:42:03 -0300

    jogo

---
+++ jogo de adivinhacao.py
@@ -0,0 +1,18 @@
+print("**********")
+print("Bem Vindo ao jogo de Adivinhação!")
+print("**********")
+
+numero_secreto = 50 
+
+chute_str = input("Digite o seu número:")
+print("Você digitou:", chute_str)
+chute = int(chute_str)
+
+if (numero_secreto == chute )
+print("Você acertou!")
+else:
+    print ("Você acertou")
+
+    print("Você errou!")
+
+    print("Fim de jogo")
